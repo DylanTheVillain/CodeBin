7080a2d93581ebd618f6d11054de6b43a736f5a0
DylanTheVillain <dylanatasmsa@gmail.com>
2014-05-02 16:20:04 -0500

    backend

--- src/backend.py
+++ src/backend.py
@@ -5,10 +5,22 @@ import sys
 
 print "Content-Type: text/html\n"
 try:
-	con=db.connect("johnsondy","localhost","johnsondy","password")
+	con=db.connect('localhost','johnsondy','password','johnsondy')
 	cur=con.cursor()
 except db.Error, e:
 	print e 
 	sys.exit(1)
 
 form=cgi.FieldStorage()
+
+if (int(form['pick'])==1):
+	cur.execute("SELECT filename FROM filetable WHERE filename=%s",str(form['file'].value))
+	con.commit()
+	fileName=cur.fetchall()
+	if (len(fileName)==0):
+		print "This file no longer exists."
+	else:
+		fi=open(str(form['file'].value)+".py","r")
+		for line in fi:
+			print line
+		fi.close()
