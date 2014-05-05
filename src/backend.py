cef00272d330f5d7dff7864a6d3058a5d25fc3b7
DylanTheVillain <dylanatasmsa@gmail.com>
2014-05-04 21:20:51 -0500

    Timestamps and shit
    Creating timestamps and logging them into html to create file names.

--- src/backend.py
+++ src/backend.py
@@ -2,6 +2,11 @@ import MySQLdb as db
 import cgi
 import os
 import sys
+import shutil
+import datetime
+import time
+import hashlib
+from random import randrange
 
 print "Content-Type: text/html\n"
 try:
@@ -13,14 +18,26 @@ except db.Error, e:
 
 form=cgi.FieldStorage()
 
-if (int(form['pick'])==1):
-	cur.execute("SELECT filename FROM filetable WHERE filename=%s",str(form['file'].value))
-	con.commit()
-	fileName=cur.fetchall()
-	if (len(fileName)==0):
-		print "This file no longer exists."
-	else:
-		fi=open(str(form['file'].value)+".txt","r")
-		for line in fi:
-			print line
-		fi.close()
+def GenFileName():
+	ti=time.time()
+	ts=datetime.datetime.fromtimestamp(ti).strftime('%Y-%m-%d %H:%M:%S')
+	tsString=str(ts)
+	tsHash=hashlib.sha224(tsString).hexdigest()
+	tsHash=str(tsHash)
+	fiName=""
+	while (True):
+		fiName=""
+		for x in range(0,10,1):
+			hashDig=randrange(0,len(tsHash)-1)
+			fiName+=tsHash[hashDig]
+		cur.execute("SELECT COUNT(filename) FROM filetable WHERE filename=%s",fiName)
+		count=con.commit()
+		if (count==None):
+			cur.execute("INSERT INTO `filetable`(`filename`, `filehash`, `lastaccess`) VALUES (%s,%s,%s)",(fiName,tsHash,ts))
+			con.commit()
+			break
+
+
+
+if (int(form['pick'].value)==1):
+	GenFileName()
