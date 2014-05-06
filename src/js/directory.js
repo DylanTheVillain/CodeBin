f4b5fd42071a431375900a00f1fe0554afee3251
Alex-Jaeger <jaeger.alex.io@gmail.com>
2014-05-06 17:15:06 -0500

    Directory Update

--- src/js/directory.js
+++ src/js/directory.js
@@ -1,19 +1,42 @@
 
 /*
-	<div class="file"><a class="file-link" href="#" onclick=""><i class="fa fa-edit file-icon"></i></a><label class="filename">Backend.py</label></div>
+	Written By: Alex Jaeger 5/5/2014
 
+	Purpose: To control the file directory on directory.html
+	Linked Files: directory.html
 */
 
-file_id=0
+/*
+	<div class="file">
+		<a class="file-link" href="#" onclick=""><i class="fa fa-square-o file-icon"></i></a>
+		<label class="filename">Backend.py</label>
+	</div>
+*/
+
+file_id=0;
+
+var selectedFiles=[];
+var files=[];
 
 function addFileListing(ul_ID, li_CLASS) {
-	
-	$("#"+ul_ID).append('<li class="directory-listing"><div class="file"><a class="file-link" href="#" onclick=""><i class="fa fa-edit file-icon"></i></a><label class="filename">Backend.py</label></div></li>');
-
+	$("#"+ul_ID).append('<li class="directory-listing" data-id='+file_id+'><div class="file"><a class="file-link" href="#" onclick="selectFileListing('+file_id+')"><i class="fa fa-square-o file-icon"></i></a><label class="filename">Backend.py</label></div></li>');
 	file_id+=1;
 }
 
 function deleteFileListing(file_ID) {
 	var li=document.querySelectorAll(""+file_ID);
 	alert(li);
+}
+
+function selectFileListing(file_ID) {
+	var selectedFile=document.getElementsByTagName('li')[file_ID];
+	var icon=selectedFile.getElementsByTagName('i')[0];
+
+	if(icon.className=="fa fa-square-o file-icon") {
+		icon.className="fa fa-check-square-o file-icon";
+	}
+	
+	else {
+		icon.className="fa fa-square-o file-icon";
+	}
 }
