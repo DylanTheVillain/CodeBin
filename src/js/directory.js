49134ff2b82bbab0f825b20026980ace4dc3d173
Alex-Jaeger <jaeger.alex.io@gmail.com>
2014-05-06 23:34:17 -0500

    Delete Function
    Its a little buggy

--- src/js/directory.js
+++ src/js/directory.js
@@ -10,6 +10,7 @@
 	<div class="file">
 		<a class="file-link" href="#" onclick=""><i class="fa fa-square-o file-icon"></i></a>
 		<label class="filename">Backend.py</label>
+		<input class="new-filename"type="text" name="filename"/>
 	</div>
 */
 
@@ -26,13 +27,26 @@ var files=[];
 	@param li_CLASS the class of the li
 */
 function addFileListing(ul_ID, li_CLASS) {
-	$("#"+ul_ID).append('<li class="directory-listing" data-id='+file_id+'><div class="file"><a class="file-link" href="#" onclick="selectFileListing('+file_id+')"><i class="fa fa-square-o file-icon"></i></a><label class="filename">Backend.py</label></div></li>');
-	file_id+=1;
+	$("#"+ul_ID).append('<li class="directory-listing" data-id='+file_id+'>\
+		<div class="file">\
+			<a class="file-link" href="#" onclick="selectFileListing('+file_id+')"><i class="fa fa-square-o file-icon"></i></a>\
+				<input class="new-filename"type="text" onKeyPress="return keyEvent(this)" name="filename"/>\
+		</div>\
+	</li>');
+
+	file_id++;
 }
 
-function deleteFileListing(file_ID) {
-	var li=document.querySelectorAll(""+file_ID);
-	alert(li);
+function deleteFileListing() {
+	for(var i=0;i<selectedFiles.length;i++) {
+		var elem=selectedFiles[i];
+		elem.parentNode.removeChild(elem);
+		
+		selectedFiles[i]=null;
+		file_id--;
+	}
+
+	return false;
 }
 
 function selectFileListing(file_ID) {
@@ -41,7 +55,28 @@ function selectFileListing(file_ID) {
 
 	if(icon.className=="fa fa-square-o file-icon") {
 		icon.className="fa fa-check-square-o file-icon";
+		selectedFiles.push(selectedFile);
 	} else {
 		icon.className="fa fa-square-o file-icon";
 	}
+}
+
+function keyEvent(e) {
+    var keycode;
+
+    if (window.event) 
+    	keycode = window.event.keyCode;
+    else if (e) 
+    	keycode = e.which;
+    else 
+    	return true;
+
+    if (keycode==13) {
+        alert("Enter");
+        //myfield.form.submit();
+        return false;
+    }
+
+    else
+        return true;
 }
