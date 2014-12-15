6f92f0f6d940535723d26878132ad3fe7608a310
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2014-12-15 17:07:31 -0600

    Rough Beginnings

--- codebin/codebin/settings.py
+++ codebin/codebin/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.editor',
+    'common',
 )
 
 MIDDLEWARE_CLASSES = (
--- codebin/codebin/urls.py
+++ codebin/codebin/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'codebin.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'common.helpers.home.Index'),
+    url(r'^editor/', include('apps.editor.urls', namespace = 'editor')),
     url(r'^admin/', include(admin.site.urls)),
 )
