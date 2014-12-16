6ab4ed5e7a25582222383ce8c80de96c8407611b
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2014-12-16 14:06:01 -0600

    Added skulpt and ace but no integration.

--- codebin/codebin/urls.py
+++ codebin/codebin/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'codebin.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'common.helpers.home.Index'),
     url(r'^editor/', include('apps.editor.urls', namespace = 'editor')),
     url(r'^admin/', include(admin.site.urls)),
