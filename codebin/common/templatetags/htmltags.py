1263c1e482b244964a1373a5c8623d4e270596d7
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2014-12-29 23:12:43 -0600

    Updated to latest version of the unescape syntax and solved the disappearing '+' sign problem with encodeURIComponent.

--- codebin/common/templatetags/htmltags.py
+++ codebin/common/templatetags/htmltags.py
@@ -1,11 +1,10 @@
 from django import template
-import html.parser
+import html
 
 register = template.Library()
 
 def html_decode(value):
-	parser = html.parser.HTMLParser()
-	decodedString = parser.unescape(value)
+	decodedString = html.unescape(value)
 	decodedString = decodedString.replace('"', '\\"')
 	decodedString = decodedString.replace("'", "\\'")
 	decodedString = decodedString.replace('\n','\\n')
