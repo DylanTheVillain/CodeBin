87130fcb4f7bd544f3f50bd403504d9f7046a3d1
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2014-12-29 22:27:02 -0600

    Added more exceptions to the html_decode. and discovered a disappering plus sign.

--- codebin/common/templatetags/htmltags.py
+++ codebin/common/templatetags/htmltags.py
@@ -7,6 +7,8 @@ def html_decode(value):
 	parser = html.parser.HTMLParser()
 	decodedString = parser.unescape(value)
 	decodedString = decodedString.replace('"', '\\"')
+	decodedString = decodedString.replace("'", "\\'")
+	decodedString = decodedString.replace('\n','\\n')
 	return decodedString
 
 register.simple_tag(html_decode)
